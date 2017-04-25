0aeb04f40a6e2cc5451e7572df55d143499e8c65
ccc <522978139@qq.com>
2017-04-25 18:02:05 +0800

    微信小程序 - 学习20170425

--- xiaoconghui/app.js
+++ xiaoconghui/app.js
@@ -1,30 +1,38 @@
 //app.js
 App({
-  onLaunch: function () {
-    //调用API从本地缓存中获取数据
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
-    var that = this
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo
-              typeof cb == "function" && cb(that.globalData.userInfo)
-            }
-          })
+    // 初始化的时候
+    onLaunch: function() {
+        //调用API从本地缓存中获取数据
+        var logs = wx.getStorageSync('logs') || []
+        logs.unshift(Date.now())
+        wx.setStorageSync('logs', logs)
+    },
+    // 小程序显示出来的时候
+    onShow:function(){
+        console.log("生命周期函数-监听小程显示的时候-从后台出现，或者打开的时候")
+    },
+    onHide:function(){
+        console.log("小程序隐藏的时候-比如在后台的时候")
+    },
+    getUserInfo: function(cb) {
+        var that = this
+        if (this.globalData.userInfo) {
+            typeof cb == "function" && cb(this.globalData.userInfo)
+        } else {
+            //调用登录接口
+            wx.login({
+                success: function() {
+                    wx.getUserInfo({
+                        success: function(res) {
+                            that.globalData.userInfo = res.userInfo
+                            typeof cb == "function" && cb(that.globalData.userInfo)
+                        }
+                    })
+                }
+            })
         }
-      })
+    },
+    globalData: {
+        userInfo: null
     }
-  },
-  globalData:{
-    userInfo:null
-  }
 })
--- xiaoconghui/pages/index/index.js
+++ xiaoconghui/pages/index/index.js
@@ -2,25 +2,35 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  }
+    data: {
+        motto: 'Hello World',
+        userInfo: {}
+    },
+    //事件处理函数
+    bindViewTap: function() {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+    changeMotto: function() {
+        //this就代表是 Page页面 对象；而不是那个触发事件的元素
+        console.log(this);
+        this.setData({
+            motto:"你好 世界",
+        });
+        wx.navigateTo({
+            url: '../test/test'
+        });
+    },
+    onLoad: function() {
+        console.log('onLoad')
+        var that = this
+            //调用应用实例的方法获取全局数据
+        app.getUserInfo(function(userInfo) {
+            //更新数据
+            that.setData({
+                userInfo: userInfo
+            })
+        })
+    }
 })
